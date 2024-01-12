562f29c79eb90ddb0ba5bba0bcf192fe95c3e149
germana05 <90755950+germana05@users.noreply.github.com>
2024-01-12 10:03:46 +0100

    t/m opdracht 43

--- OpdrachtenBoek/Program.cs
+++ OpdrachtenBoek/Program.cs
@@ -8,8 +8,30 @@ namespace OpdrachtenBoek
 {
     class Program
     {
+        //opdracht 40
+        static int Int = 1;
+        static char Char;
+        static string String = "2";
+
+        //opdracht 41
+        static char Char2 = '3';
+        static int Int2;
+        static int Int3;
+
+        //opdracht 42
+        static char Char3 = '4';
+        static int Int4;
+
         static void Main(string[] args)
         {
+
+            //opdracht 41
+            Char = (char)Int32.Parse(Int.ToString());
+            Int2 = Int32.Parse(Char2.ToString());
+            Int3 = Int32.Parse(String);
+
+            //opdracht 43
+
         }
     }
 }
